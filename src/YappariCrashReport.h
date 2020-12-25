b8bd025476cb0abca641e58572454268d7e24b28
Naikel Aparicio <naikel@gmail.com>
2020-12-24 20:40:33 -0500

    Added a default standard license

--- src/YappariCrashReport.h
+++ src/YappariCrashReport.h
@@ -1,5 +1,35 @@
-#ifndef ASMCRASHREPORT_H
-#define ASMCRASHREPORT_H
+/*
+ * Copyright (C) 2017, 2020 Andy Maloney, Naikel Aparicio. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the author and should not be interpreted as representing
+ * official policies, either expressed or implied, of the copyright holder.
+ */
+
+
+#ifndef YAPPARICRASHREPORT_H
+#define YAPPARICRASHREPORT_H
 
 #include <QString>
 
@@ -10,7 +40,7 @@ namespace YappariCrashReport {
    /// @param inCrashReport The report including the stack trace as a QString
    using crashReportCallback = void (*)(const QString &);
 
-   ///! Set a signal handler to capture stack trace to a log file.
+   /// Set a signal handler to capture stack trace to a log file.
    ///
    /// @param inCrashReportCallback A callback function to call after we've shown the dialog to the user
    void setSignalHandler( crashReportCallback inCrashReportCallback = nullptr );
